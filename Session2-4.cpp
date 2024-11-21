650c7cfbbcc50761f0ba43e17422d0c772da511f
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 08:54:01 +0700

    Upload bài Lập trình C cơ bản Session2-4

---
+++ Session2-4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int canh = 10;
+    printf("Canh la %d \n",canh);    
+    int chu_vi = 5 * canh;    
+    printf("Chu vi la %d\n",chu_vi);   
+    int dien_tich = canh * canh;
+    printf("Dien tich la %d",dien_tich);
+	return 0;
+}
