5d17297d90ba02af6713ef77bd55bc684bc60ce7
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 12:11:52 +0700

    Update and rename Session2-5.cpp to Session2-5.c

--- Session2-5.c
+++ Session2-5.c
@@ -3,7 +3,7 @@ int main(){
 // Khai bao hai bien chieu dai va chieu rong
    int lengt = 10;
    printf("Chieu dai la %d cm\n",lengt);
-   int width = 5;
+   int width = 4;
    printf("Chieu rong la %d cm\n",width);
 // Tinh chu vi va dien tich
     int chu_vi = 2 * (lengt+width);
