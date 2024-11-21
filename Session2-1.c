0fd1b314e19c0b004f7fb3a1966413967bf00db6
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 11:44:11 +0700

    Upload bài lập trình C cơ bản Session2-1

---
+++ Session2-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+// khoi tao hai so nguyen
+   int firstnumber = 5;
+   int secondnumber = 10;
+//  tinh tong va luu ket qua vao bien khac
+   int total = firstnumber + secondnumber;
+//  ket qua
+   printf("tong cua firstnumber va secondnumber la: %d\n",total);
+	return 0;
+}
