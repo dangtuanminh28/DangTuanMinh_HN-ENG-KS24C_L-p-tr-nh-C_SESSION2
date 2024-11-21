4add056f7f0cea83e5553972f2979716cd953ce3
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 12:55:15 +0700

    Update Session2-6.c

--- Session2-6.c
+++ Session2-6.c
@@ -3,7 +3,7 @@ int main(){
   const float PI = 3.14;
   int ban_kinh = 6;
   printf("Ban kinh hinh tron la %d\n", ban_kinh);
-  float chu_vi = Pi * ban_kinh * 2;
+  float chu_vi = PI * ban_kinh * 2;
   printf("Chu vi hinh tron la %.2f\n", chu_vi);
   float dien_tich = PI*(ban_kinh*ban_kinh);
   printf("Dien tich hinh tron la %.2f", dien_tich);
