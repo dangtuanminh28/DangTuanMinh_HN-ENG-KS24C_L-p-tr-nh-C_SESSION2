c9a641ba6e19593bd6ec809a4e9982998457ddc4
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 11:52:31 +0700

    Upload bài Lập trình C cơ bản Session2-6

---
+++ Session2-6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+//khai bao bien PI float ban kinh
+	float Pi = 3.14;
+	int ban_kinh  = 5;
+	//tinh chu vi va dien tich hinh tron
+	int Perimeter  = Pi * ban_kinh * 2;
+	printf(" chu vi hinh tron la %d\n",Perimeter );
+	int Acreage = Pi * ban_kinh * ban_kinh;
+	printf(" dien tich hinh tron la %d\n",Acreage);
+	return 0;
+}
