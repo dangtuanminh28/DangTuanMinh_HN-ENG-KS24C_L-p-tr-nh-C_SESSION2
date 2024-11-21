581930ea75293f664c4e396237252ebcc2c7b904
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 12:10:11 +0700

    Update Session2-2.c

--- Session2-2.c
+++ Session2-2.c
@@ -4,8 +4,8 @@ int main(){
    int firstNumber = 5;
    int secondNumber = 10;
 //  tinh tong va luu ket qua vao bien khac
-   int firstTotal = firstNumber + secondNumber;
+   int total = firstNumber + secondNumber;
 //  ket qua
-   printf("tong cua firstnumber va secondnumber la: %d\n",firstTotal);
+   printf("tong cua firstnumber va secondnumber la: %d\n",total);
 	return 0;
 }
