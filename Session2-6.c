ae902d1625230b4851385397e4a7d44873c9fa7b
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 12:24:06 +0700

    Update Session2-6.c

--- Session2-6.c
+++ Session2-6.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main(){
-//khai bao bien PI float ban kinh
-	const float Pi = 3.14;
-	int ban_kinh  = 5;
-	//tinh chu vi va dien tich hinh tron
-	int Perimeter  = Pi * ban_kinh * 2;
-	printf(" chu vi hinh tron la %d\n",Perimeter );
-	int Acreage = Pi * ban_kinh * ban_kinh;
-	printf(" dien tich hinh tron la %d\n",Acreage);
-	return 0;
+  const float PI = 3.14;
+  int ban_kinh = 6;
+  printf("Ban kinh hinh tron la %d\n", ban_kinh);
+  float chu_vi = Pi * ban_kinh * 2;
+  printf("Chu vi hinh tron la %.2f\n", Perimeter);
+  float dien_tich = PI*(ban_kinh*ban_kinh);
+  printf("Dien tich hinh tron la %.2f", Acreage);
+  return 0;
 }
